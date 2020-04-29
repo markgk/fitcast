c00096d4e93a1584f6a52835e3a65aee4e7cfad7
Mark Kremer <mark@marks-imac-3.attlocal.net>
2020-04-29 13:52:57 -0700

    First commit

--- src/Fasctories/Factory.ts
+++
@@ -1,8 +0,0 @@
-import { Exercise, Subscriber, Program } from '../Types/GymTypes';
-
-export const makeExersize = (e: Exercise, s: Subscriber): Exercise => {
-  return e;
-};
-export const makeProgram = (p: Program, s: Subscriber): Program => {
-  return p;
-};
