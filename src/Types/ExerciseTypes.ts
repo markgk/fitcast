a508ef1760e8763da0039f620d41d6c09379eb74
Mark Kremer <mark@marks-imac-3.attlocal.net>
2020-04-29 17:07:51 -0700

    Addded commnets to clarify defaults

--- src/Types/ExerciseTypes.ts
+++ src/Types/ExerciseTypes.ts
@@ -3,19 +3,19 @@ import { Media } from './MediaTypes';
 interface WorkoutObject {
   name: string;
   uid: string;
-  description: string;
-  tags: string[];
-  dos: string[];
-  donts: string[];
-  media: Media;
+  description: string; // '' for null
+  tags: string[]; // [] for none
+  dos: string[]; // [] for none
+  donts: string[]; // [] for none
+  media: Media; // default meadia if not specified
 }
 
 export interface Position extends WorkoutObject {
-  count: number; // how long to hold the position
+  count: number; // how long to hold the position, defaulsts to 1
 }
 
 export interface Transition extends WorkoutObject {
-  count: number; // number of steps in transition
+  count: number; // number of steps in transition, defaults ot 1
 }
 
 export interface Move {
@@ -33,7 +33,7 @@ export interface Exercise extends WorkoutObject {
   equipment: Equipment;
 }
 export interface Break extends WorkoutObject {
-  duration: number; // duration in seconds
+  duration: number; // duration in seconds, defaults to 120
 }
 
 export interface Set {
@@ -43,7 +43,7 @@ export interface Set {
 }
 
 export interface Workout extends WorkoutObject {
-  warmUp: Exercise[];
+  warmUp: Exercise[]; // [] stands for none
   sets: Set[];
-  warmDown: Exercise[];
+  warmDown: Exercise[]; // [] stands for none
 }
